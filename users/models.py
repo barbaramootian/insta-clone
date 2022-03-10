d480c7172e65cdccd0f9431324d2e8c76a568de0
barbaramootian <barbara.mootian@student.moringaschool.com>
2022-03-10 17:13:51 +0300

    user models

--- users/models.py
+++ users/models.py
@@ -1,20 +1,16 @@
-# # User models
 
-# # Django
-# from django.contrib.auth.models import User
-# from django.db import models
+from django.contrib.auth.models import User
+from django.db import models
 
-# # Create your models here.
+# Create your models here.
+class Profile(models.Model):
+    picture = models.ImageField( upload_to='users/pictures', blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    caption = models.CharField(max_length=250, blank=True)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True, )
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
 
-
-# class Profile(models.Model):
-#     picture = models.ImageField( upload_to='users/pictures', blank=True, null=True)
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     caption = models.CharField(max_length=250, blank=True)
-#     likes = models.ManyToManyField(User, related_name='likes', blank=True, )
-#     created = models.DateTimeField(auto_now_add=True)
-#     modified = models.DateTimeField(auto_now=True)
-
-#     def __str__(self):
-#         """Return username"""
-#         return self.user.username
+    def __str__(self):
+        """Return username"""
+        return self.user.username
