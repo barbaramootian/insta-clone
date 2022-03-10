f3570a8d389163d3769f36328dcd8bdcc5c13487
barbaramootian <barbara.mootian@student.moringaschool.com>
2022-03-10 17:13:07 +0300

    user form fo authenitaction

--- users/forms.py
+++ users/forms.py
@@ -1,62 +1,54 @@
 
-# """Users Forms."""
+from django import forms
+from django.contrib.auth.models import User
+from users.models import Profile
 
-# # Django
-# from django import forms
+class SignupForm(forms.Form):
+    username = forms.CharField(
+        min_length=5,
+        max_length=50,
+        widget=forms.TextInput(attrs={'placeholder': 'Username','class': 'form-control'})
+    )
+    password = forms.CharField(
+        min_length=8,
+        max_length=70,
+        widget=forms.PasswordInput(attrs={'placeholder': 'Password','class': 'form-control'})
+    )
+    password_confirmation = forms.CharField(
+        min_length=8,
+        max_length=70,
+        widget=forms.PasswordInput(attrs={'placeholder': 'Password Confirmation','class': 'form-control'})
+    )
+    email = forms.CharField(
+        min_length=7,
+        max_length=70,
+        widget=forms.EmailInput(attrs={'placeholder': 'Email','class': 'form-control'})
+    )
 
-# # Models
-# from django.contrib.auth.models import User
-# from users.models import Profile
+    def clean_username(self):
+        """Username must be unique"""
+        username = self.cleaned_data['username']
+        username_taken = User.objects.filter(username=username).exists()
+        if username_taken:
+            raise forms.ValidationError('Username is already in use.')
+        return username
 
-# class SignupForm(forms.Form):
-#     """Signup form"""
+    def clean(self):
+        """Verify password confirmation match."""
+        data = super().clean()
 
-#     # Por default los valores son requeridos
-#     username = forms.CharField(
-#         min_length=5,
-#         max_length=50,
-#         widget=forms.TextInput(attrs={'placeholder': 'Username','class': 'form-control'})
-#     )
-#     password = forms.CharField(
-#         min_length=8,
-#         max_length=70,
-#         widget=forms.PasswordInput(attrs={'placeholder': 'Password','class': 'form-control'})
-#     )
-#     password_confirmation = forms.CharField(
-#         min_length=8,
-#         max_length=70,
-#         widget=forms.PasswordInput(attrs={'placeholder': 'Password Confirmation','class': 'form-control'})
-#     )
-#     email = forms.CharField(
-#         min_length=7,
-#         max_length=70,
-#         widget=forms.EmailInput(attrs={'placeholder': 'Email','class': 'form-control'})
-#     )
+        password = data['password']
+        password_confirmation = data['password_confirmation']
 
-#     def clean_username(self):
-#         """Username must be unique"""
-#         username = self.cleaned_data['username']
-#         username_taken = User.objects.filter(username=username).exists()
-#         if username_taken:
-#             raise forms.ValidationError('Username is already in use.')
-#         return username
+        if password != password_confirmation:
+            raise forms.ValidationError('Passwords do not match.')
+        return data
 
-#     def clean(self):
-#         """Verify password confirmation match."""
-#         data = super().clean()
+    def save(self):
+        """Create user and profile"""
+        data = self.cleaned_data
+        data.pop('password_confirmation')
 
-#         password = data['password']
-#         password_confirmation = data['password_confirmation']
-
-#         if password != password_confirmation:
-#             raise forms.ValidationError('Passwords do not match.')
-#         return data
-
-#     def save(self):
-#         """Create user and profile"""
-#         data = self.cleaned_data
-#         data.pop('password_confirmation')
-
-#         user = User.objects.create_user(**data)
-#         profile = Profile(user=user)
-#         profile.save()
+        user = User.objects.create_user(**data)
+        profile = Profile(user=user)
+        profile.save()
